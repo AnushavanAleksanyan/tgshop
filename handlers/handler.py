2fed50840dab83fc35c237c8c3f31dbedf5b8419
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2023-09-30 18:03:54 +0400

    comit 30.09.23

--- handlers/handler.py
+++ handlers/handler.py
@@ -1,12 +1,19 @@
+# импортируем библиотеку abc для реализации абстрактных классов
 import abc
-
+# импортируем разметку клавиатуры и клавиш
 from markup.markup import Keyboards
+# импортируем класс-менеджер для работы с библиотекой
 from data_base.dbalchemy import DBManager
 
+
 class Handler(metaclass=abc.ABCMeta):
+
     def __init__(self, bot):
+        # получаем объект бота
         self.bot = bot
-        self.keyboards = Keyboards()
+        # инициализируем разметку кнопок
+        self.keybords = Keyboards()
+        # инициализируем менеджер для работы с БД
         self.BD = DBManager()
 
     @abc.abstractmethod
--- settings/config.py
+++ settings/config.py
@@ -1,38 +1,51 @@
 import os
 from emoji import emojize
 
-TOKEN = ""
-
-NAME_DB ='products.sqlite'
-
+TOKEN = "6401415271:AAHot77zVjngwpEi8bGHaX3onG2URlp-MDI"
+# название БД
+NAME_DB = 'products.sqlite'
+# версия приложения
 VERSION = '0.0.1'
-
+# автор приложения
 AUTHOR = 'User'
 
-BASE_DIR = os.path.dirname(os.path.abspath((__file__)))
-
+# родительская директория
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# путь до базы данных
 DATABASE = os.path.join('sqlite:///'+BASE_DIR, NAME_DB)
 
 COUNT = 0
 
+# кнопки управления
 KEYBOARD = {
-    'CHOOSE_GOODS': emojize(':open_file_folder: Ընտրել ապրանքը'),
-    'INFO': emojize(':speach_baloon: Խանութի մասին'),
-    'SETTINGS': emojize('⚙ Կարգավորումներ'),
-    'SEMIPRODUCT': emojize(':pizza: Կիսաֆաբրիկատներ'),
-    'GROCERY': emojize(':bread: Հացաբուլկեղեն'),
-    'ICE_CREAM': emojize(':shaved_ice: Պաղպաղակ'),
+    'CHOOSE_GOODS': emojize(':open_file_folder: Выбрать товар'),
+    'INFO': emojize(':speech_balloon: О магазине'),
+    'SETTINGS': emojize('⚙️ Настройки'),
+    'SEMIPRODUCT': emojize(':pizza: Полуфабрикаты'),
+    'GROCERY': emojize(':bread: Бакалея'),
+    'ICE_CREAM': emojize(':shaved_ice: Мороженое'),
+    '<<': emojize('⏪'),
+    '>>': emojize('⏩'),
+    'BACK_STEP': emojize('◀️'),
+    'NEXT_STEP': emojize('▶️'),
+    'ORDER': emojize('✅ ЗАКАЗ'),
+    'X': emojize('❌'),
+    'DOUWN': emojize('🔽'),
     'AMOUNT_PRODUCT': COUNT,
     'AMOUNT_ORDERS': COUNT,
-    'APPLY': '✅ Հաստատել պատվերը',
+    'UP': emojize('🔼'),
+    'APPLAY': '✅ Оформить заказ',
+    'COPY': '©️'
 }
 
+# id категорий продуктов
 CATEGORY = {
     'SEMIPRODUCT': 1,
     'GROCERY': 2,
     'ICE_CREAM': 3,
 }
 
+# названия команд
 COMMANDS = {
     'START': "start",
     'HELP': "help",
